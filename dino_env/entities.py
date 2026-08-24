f8f7bbc7c9d78ece93ffdbf9453de4442fa5b152
Ghiwane <sehaki.ghiwane@gmail.com>
2026-08-24 18:53:27 +0200

    Add obstacle classes for Cactus and Ptero with random generation functionality

--- dino_env/entities.py
+++ dino_env/entities.py
@@ -1,5 +1,6 @@
 import constants as ct
 from enum import Enum, auto
+import random
 
 class State(Enum):
     RUNNING = auto()
@@ -63,3 +64,76 @@ class Dino:
 
             y0 = self.dino_y + y_offset
             return (ct.DINO_X, y0, ct.DINO_X + 118, y0 + duck_height)
+
+class Obstacle:
+    def __init__(self, x, y, width, height, obstacle_type):
+        self.X = x
+        self.Y = y
+        self.width = width
+        self.height = height
+        self.type = obstacle_type
+
+    def update(self,speed):
+        self.X -= speed
+
+    def is_off_screen(self):
+        if self.X + self.width < 0:
+            return True
+        else:
+            return False
+
+    def get_hitbox(self):
+        return (self.X, self.Y, self.X + self.width, self.Y + self.height)
+
+class Cactus(Obstacle):
+    VARIANTS = [
+        (ct.BIGCACTUS_H, ct.BIGCACTUS1_W),
+        (ct.BIGCACTUS_H, ct.BIGCACTUS2_W),
+        (ct.BIGCACTUS_H, ct.BIGCACTUS3_W),
+        (ct.BIGCACTUS_H, ct.BIGCACTUS4_W),
+
+        (ct.SMALLCACTUS_H, ct.SMALLCACTUS1_W),
+        (ct.SMALLCACTUS_H, ct.SMALLCACTUS2_W),
+        (ct.SMALLCACTUS_H, ct.SMALLCACTUS3_W),
+    ]
+
+
+    def __init__(self, variant_idx):
+
+        self.variant_index = variant_idx
+        height, width = self.VARIANTS[variant_idx]
+
+        super().__init__(
+        x = ct.SCREEN_W,
+        y = ct.CACTUS_Y,
+        width = width,
+        height = height,
+        obstacle_type = "cactus"
+    )
+
+    @classmethod
+    def create_random(cls):
+        random_variant = random.randint(0, len(cls.VARIANTS) - 1)
+        return cls(variant_idx = random_variant)
+
+class Ptero(Obstacle):
+    HEIGHTS_BY_TYPE = {
+        "P_LOW":  ct.PTERO_Y_LOW,
+        "P_MID":  ct.PTERO_Y_MID,
+        "P_HIGH": ct.PTERO_Y_HIGH
+    }
+
+    def __init__(self, obstacle_type, width = ct.PTERO_W, height = ct.PTERO_H):
+        y_position = self.HEIGHTS_BY_TYPE.get(obstacle_type, self.HEIGHTS_BY_TYPE["P_MID"])
+        super().__init__(
+                x = ct.SCREEN_W,
+                y = y_position,
+                width = width,
+                height = height,
+                obstacle_type = obstacle_type
+            )
+
+    @classmethod
+    def create_random(cls):
+        ptero_type = random.choice(["P_LOW", "P_MID", "P_HIGH"])
+        return cls(obstacle_type=ptero_type)
